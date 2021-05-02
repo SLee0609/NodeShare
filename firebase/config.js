870ad7de0a0ab1d898f08da19aee65c5e7884a34
derek yuan <dyuangm@gmail.com>
2021-05-01 22:37:41 -0400

    connected app to firebase and set up registration

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD2beqmwMy231hJQL350rx4TjdrOGc5S2M',
+  authDomain: 'lclink.firebaseapp.com',
+  databaseURL: 'https://lclink-default-rtdb.firebaseio.com/',
+  projectId: 'lclink',
+  storageBucket: 'lclink.appspot.com',
+  messagingSenderId: '569767698533',
+  appId: '1:569767698533:ios:ffa5c453dec18a6033a68c',
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
