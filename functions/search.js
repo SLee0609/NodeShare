e555b46b65135566ddbdeebc4e5dbfa6f44130db
henrydeng <henry_deng@loomis.org>
2021-08-14 16:48:20 +0800

    added basic relevancy check and capitalization tolerance

--- functions/search.js
+++ functions/search.js
@@ -6,14 +6,23 @@ import Post from "../models/post.js";
 let searchByDate = async (posts, keyword) => {
     // split the keyword string into individual words
     const keywordArr = keyword.split(" ");
+    // lowercase all words so capitalization does not matter
+    var lowercaseKeyword = [];
+    keywordArr.forEach((word) => {
+        lowercaseKeyword.push(word.toLowerCase());
+    });
     // make the post content into strings
     const postArr = await stringifyPost(posts);
     // find which posts contain one or more of the keywords
-    const matchingPosts = await findKeywords(keywordArr, postArr);
+    const matchingPosts = await findKeywords(lowercaseKeyword, postArr);
     // find the postIDs of the posts
     const postIDs = await findPostID(matchingPosts, posts);
     // return the postIDs (could change to returning the post object)
     return postIDs;
+}
+
+let searchByRelevance = async (posts, keyword) => {
+
 }
 
 // make the post content and title into an 2D array of strings (add user later)
@@ -21,7 +30,12 @@ let stringifyPost = async (posts) => {
     var postStr = [];
     posts.forEach((post) => {
         var str = post.title.split(" ").concat(post.description.split(" "));
-        postStr.push(str);
+        var lowerStr = [];
+        // lowercase all words
+        str.forEach((word) => {
+           lowerStr.push(word.toLowerCase());
+        });
+        postStr.push(lowerStr);
     });
     return postStr;
 }
@@ -30,18 +44,24 @@ let findKeywords = async (keyword, posts) => {
     var postIndex = [];
     // find matching keywords and post content
     for (var i = 0; i < posts.length; i++) {
-        posts[i].forEach((string) => {
-            keyword.forEach((word) => {
-                if (word == string) {
-                    postIndex.push(i);
+        var relevance = 0;
+        for (var j = 0; j < keyword.length; j++) {
+            for (var k = 0; k < posts[i].length; k++) {
+                if (keyword[j] == posts[i][k]) {
+                    // checks if a single keyword is contained within the post
+                    relevance ++;
+                    break;
                 }
-            });
-        });
+            }
+        }
+
+        // if more than 2/3 of the keywords are in the post, return the post
+        if (relevance / keyword.length >= 2/3) {
+            postIndex.push(i);
+        }
     }
-    // remove duplicates
-    var postIndexUnique = [...new Set(postIndex)];
     // returns the position of the post in the original post array
-    return postIndexUnique;
+    return postIndex;
 }
 
 // use the post index to find the postid
@@ -60,7 +80,7 @@ let test = async () => {
     var post2 = new Post("post2", "categories", "this is the new title", "123uid", "imageuri", "this is the new post description", "some time");
     postArr.push(post1);
     postArr.push(post2);
-    var x = await searchByDate(postArr, "this");
+    var x = await searchByDate(postArr, "THIS IS");
     console.log(x);
 }
 
