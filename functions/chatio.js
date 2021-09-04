0546311d92717b2ac9b5f1f55972d1ae4bef8ccc
henrydeng <henry_deng@loomis.org>
2021-09-04 17:49:41 -0400

    fixed all known notification issues

--- functions/chatio.js
+++ functions/chatio.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import { sendChatNotificationMessage } from "./notifications.js";
 
 let storeMessage = async (chatID, uid1, uid2, message) => {
-  await sendChatNotificationMessage(uid2, message);
+  await sendChatNotificationMessage(uid1, uid2, message);
   const chatref = firebase.firestore().collection("chats").doc(chatID);
   await chatref.get().then((docSnapshot) => {
     chatref.set({ lastmessage: message });
